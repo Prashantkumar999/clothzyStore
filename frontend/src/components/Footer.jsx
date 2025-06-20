7928554d9f93c1bfc8d2b341de914ef03d2b9c6c
Prashantkumar999 <prashant047alg@gmail.com>
2025-06-20 21:28:42 +0530

    commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
@@ -7,17 +7,9 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-section">
           <h3>About Us</h3>
-          <p>Your trusted destination for quality fashion and lifestyle products.</p>
-        </div>
-        
-        <div className="footer-section">
-          <h3>Quick Links</h3>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/products">Products</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
+          <p>
+            Your trusted destination for quality fashion and lifestyle products.
+          </p>
         </div>
 
         <div className="footer-section">
@@ -32,18 +24,26 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Follow Us</h3>
           <div className="social-links">
-            <a href="#" className="social-link">Facebook</a>
-            <a href="#" className="social-link">Instagram</a>
-            <a href="#" className="social-link">Twitter</a>
+            <a href="#" className="social-link">
+              Facebook
+            </a>
+            <a href="#" className="social-link">
+              Instagram
+            </a>
+            <a href="#" className="social-link">
+              Twitter
+            </a>
           </div>
         </div>
       </div>
-      
+
       <div className="footer-bottom">
-        <p>&copy; {new Date().getFullYear()} Clothzy Store. All rights reserved.</p>
+        <p>
+          &copy; {new Date().getFullYear()} Clothzy Store. All rights reserved.
+        </p>
       </div>
     </footer>
   );
 };
 
-export default Footer; 
+export default Footer;
